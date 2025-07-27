fa552013c74623f7985e7d5a08d34adaf81bfd8f
Shahryar Zainaee <hamidzne.9499@gmail.com>
2025-07-27 14:49:27 -0400

    Update script.js

--- Snake/script.js
+++ Snake/script.js
@@ -1,10 +1,8 @@
-var canvas = document.getElementById('game');
-var context = canvas.getContext('2d');
-
-var grid = 16;
-var count = 0;
-
-var snake = {
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+let grid = 16;
+let count = 0;
+let snake = {
   x: 160,
   y: 160,
   dx: grid,
@@ -12,10 +10,7 @@ var snake = {
   cells: [],
   maxCells: 4
 };
-var apple = {
-  x: 320,
-  y: 320
-};
+let apple = { x: 320, y: 320 };
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -23,44 +18,33 @@ function getRandomInt(min, max) {
 
 function loop() {
   requestAnimationFrame(loop);
-  if (++count < 7) return;
-
+  if (++count < 6) return;
   count = 0;
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   snake.x += snake.dx;
   snake.y += snake.dy;
-
   if (snake.x < 0) snake.x = canvas.width - grid;
   else if (snake.x >= canvas.width) snake.x = 0;
   if (snake.y < 0) snake.y = canvas.height - grid;
   else if (snake.y >= canvas.height) snake.y = 0;
-
   snake.cells.unshift({ x: snake.x, y: snake.y });
   if (snake.cells.length > snake.maxCells) snake.cells.pop();
 
-  context.fillStyle = 'red';
-  context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
-
-  context.fillStyle = 'green';
-  snake.cells.forEach(function (cell, index) {
-    context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
-
+  ctx.fillStyle = 'red';
+  ctx.fillRect(apple.x, apple.y, grid - 1, grid - 1);
+  ctx.fillStyle = 'green';
+  snake.cells.forEach((cell, index) => {
+    ctx.fillRect(cell.x, cell.y, grid - 1, grid - 1);
     if (cell.x === apple.x && cell.y === apple.y) {
       snake.maxCells++;
       apple.x = getRandomInt(0, 25) * grid;
       apple.y = getRandomInt(0, 25) * grid;
     }
-
-    for (var i = index + 1; i < snake.cells.length; i++) {
+    for (let i = index + 1; i < snake.cells.length; i++) {
       if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-        snake.x = 160;
-        snake.y = 160;
-        snake.cells = [];
-        snake.maxCells = 4;
-        snake.dx = grid;
-        snake.dy = 0;
-
+        snake.x = 160; snake.y = 160;
+        snake.cells = []; snake.maxCells = 4;
+        snake.dx = grid; snake.dy = 0;
         apple.x = getRandomInt(0, 25) * grid;
         apple.y = getRandomInt(0, 25) * grid;
       }
